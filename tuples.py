4e93ad1ef308b6fdfa6d67374e9a1512ff5f6f79
Ali Akbar Khaled <aliishmam23@gmail.com>
2025-10-22 20:12:53 +0600

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,9 @@
+Students = (
+    "Leonel Messi",
+    "Luis Suarez",
+    "Neymar junior"
+)
+print(Students[0:]) #It will show first to last all value
+print(Students[1]) #Luis Suarez
+print(Students[0]) #Leonel Messi
+print(Students[2]) #Neymar Junior 
