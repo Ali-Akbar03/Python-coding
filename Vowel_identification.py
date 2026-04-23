6e412476630b1f425abca4c7b4d65e987d14779e
Ali Akbar Khaled <aliishmam23@gmail.com>
2026-04-23 22:01:24 +0600

    Add files via upload

---
+++ Vowel_identification.py
@@ -0,0 +1,7 @@
+ch = 'a'
+if ch=='a' or ch=='e' or ch=='i' or ch=='o'or ch=='u' or ch=='A' or ch=='E' or ch=='E' or ch=='O'or ch=='U':
+    print(f"{ch} is a vowel")
+else:
+    print("Consonent") 
+
+
