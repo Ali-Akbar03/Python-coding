ef6472e333bacf6640f994384f7449b3e1ea5806
Ali Akbar Khaled <aliishmam23@gmail.com>
2026-04-23 21:24:03 +0600

    Update EvenOdd.py

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,4 +1,4 @@
-a =int(input("Enter the value of a to check if it is even ot odd - "))
+a =int(input("Enter the value of a to check if it is even or odd - "))
 
 if a%2 == 0:
     print(f"{a} is an even number")
