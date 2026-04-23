db1c2bbd6052dc402516a949706a1102ea8c112d
Ali Akbar Khaled <aliishmam23@gmail.com>
2026-04-23 21:04:44 +0600

    Add files via upload

---
+++ python_basic_math_and_builtins.py
@@ -0,0 +1,18 @@
+import math
+print(max(20,3)) #built in 
+print(min(20,3)) #built in 
+print(abs(-10000)) #built in 
+print(pow(2,3)) #built in 
+
+#Math module functions (need import math)
+print(math.sqrt(25))
+print(math.pi)
+print(round(3.5)) #output is 4
+print(round(3.49))  # output is 3
+print(math.ceil(3.2))
+print(math.ceil(3.5))
+print(math.floor(3.5))
+print(math.floor(-3.5))
+print(math.floor(3.5))
+print(math.floor(3.2))
+print(math.factorial(5))
