98f9255f86004e78b217a626b83fd6cc2806837e
Ali Akbar Khaled <aliishmam23@gmail.com>
2026-04-23 20:48:05 +0600

    Add files via upload

---
+++ CylinderSurfaceArea.py
@@ -0,0 +1,7 @@
+import math
+r = float((input("Enter the radius of the cylinder = ")))
+h = float((input("Enter the height of the cylinder = ")))
+Area = 2 * math.pi * r**2 + 2 * math.pi * r * h
+
+print("The Surface area of the cylinder is = ",Area)
+
